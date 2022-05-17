14cd454ad9d8caa76a055afb6984ef6f2bbfee2b
soueuwilliam <camposwilliam23@gmail.com>
2022-05-16 21:43:26 -0300

    Adição de certificados adquirido na bootcamp e projeto feito durante o curso

---
+++ Cursos/Primeiros passos para o desenvolvimento Web/Projetos/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Meu primeiro site em php!wooohoo !!</title>
+	</head>
+	<body>
+		<?php 
+			for($i = 0;$i<10; $i++){
+				print("Linha numero: " .$i . "<br />");	
+			}	
+		?>
+		
+	</body>
+<html>
